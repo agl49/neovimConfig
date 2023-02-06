f44231a133ce897aca8b42429d04734bc1c9a604
agl49 <al86568@gmail.com>
2023-02-06 08:17:29 -0600

    transparent background

--- after/plugin/color.lua
+++ after/plugin/color.lua
@@ -2,5 +2,35 @@
 vim.cmd [[colorscheme moonfly]]
 vim.g.moonflyCursorColor = true --This is not working....
 
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
